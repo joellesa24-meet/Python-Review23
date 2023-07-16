b69f97fc05ba0aeec0fcf315ba8597b0d3ba0c0b
joellesa24-meet <jsabaj20062@gmail.com>
2023-07-16 17:14:18 +0300

    its not done yet

---
+++ reviiew.py
@@ -0,0 +1,19 @@
+def create_youtube_video(title,description):
+	new_youtube_video ={ "title": " shopping haul","description": "idk","likes": 0, "dislikes": 0,{"username": " "}}
+	return new_youtube_video
+
+def like (video):
+	if "likes" in video:
+		video.append("likes"+1)
+	return("video")	
+
+def dislike(video):
+	
+	if "dislikes" in new_youtube_video:
+		new_youtube_video.append("dislikes"+1)
+
+def add_comment
+
+video = create_youtube_video
+like(video)
+
