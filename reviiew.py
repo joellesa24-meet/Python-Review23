c903be1475645189b48aa72215f26d3711c60abd
joellesa24-meet <jsabaj20062@gmail.com>
2023-07-17 18:29:04 +0300

    updated

--- reviiew.py
+++ reviiew.py
@@ -1,19 +1,20 @@
 def create_youtube_video(title,description):
-	new_youtube_video ={ "title": " shopping haul","description": "idk","likes": 0, "dislikes": 0,{"username": " "}}
+	new_youtube_video ={ "title": " shopping haul","description": "idk","likes": 0, "dislikes": 0,"comments" :{}}
 	return new_youtube_video
 
-def like (video):
-	if "likes" in video:
-		video.append("likes"+1)
-	return("video")	
+def like (new_youtube_video):
+	if "likes" in new_youtube_video:
+		new_youtube_video["likes"]+1
+	return new_youtube_video 
 
-def dislike(video):
+def dislike(new_youtube_video):
 	
 	if "dislikes" in new_youtube_video:
-		new_youtube_video.append("dislikes"+1)
+		new_youtube_video["dislikes"]+1
 
-def add_comment
+def add_comment(new_youtube_video,username,comment_text):
+	new_youtube_video["comments"][username]=comment_text
+	return new_youtube_video
 
-video = create_youtube_video
-like(video)
-
+lalala=new_youtube_video("disgusting","a person eating mushrooms")
+new_youtube_video["likes"]+495
